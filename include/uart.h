3c9e0c8d93522a3bccdefe055f8c94115dd03fe1
Aleksi Noro <aleksinoro@gmail.com>
2025-04-14 18:52:59 +0300

    modify uart headers

--- include/uart.h
+++ include/uart.h
@@ -1,7 +1,9 @@
 #ifndef UART_H
 #define UART_H
 
-bool write_uart(uart_inst_t *uart, const uint8_t *message, int *state);
-void read_uart(uart_inst_t *uart, int *state);
+bool write_uart(uart_inst_t *uart, const uint8_t *message);
+void read_at_command(uart_inst_t *uart, int *step);
+void read_ver_command(uart_inst_t *uart, int *step);
+void read_deveui_command(uart_inst_t *uart, int *step);
 
 #endif
