f5a6ff10e6aa973db85569ea6ef32d80a645ac86
Aleksi Noro <aleksinoro@gmail.com>
2025-04-14 18:52:46 +0300

    headers for str_utils

---
+++ include/str_utils.h
@@ -0,0 +1,8 @@
+#ifndef STR_UTILS_H
+#define STR_UTILS_H
+
+int split_string(char *str, const char *delimeter, char *output_arr[], int max_count);
+void remove_char(char *str, const char chr);
+void lower_case(char *str);
+
+#endif
